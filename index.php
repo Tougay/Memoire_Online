1de8d2b5d2a92908c7f01bc52cbd3f13fdd68d87
unknown <hassanahmat41gmail.com>
2024-05-31 11:36:19 +0100

    Partie du mail

--- index.php
+++ index.php
@@ -138,6 +138,16 @@ $memoires = $stmt->fetchAll();
             background-color: #003366;
             color: white;
         }
+        /* CSS Reset for footer */
+        footer, #footer {
+            display: block;
+            margin-block-start: 1em;
+            margin-block-end: 1em;
+            margin-inline-start: 0px;
+            margin-inline-end: 0px;
+            padding: 0;
+        }
+        
         #footer {
             text-align: center;
             padding: 20px;
@@ -175,14 +185,14 @@ $memoires = $stmt->fetchAll();
         }
         window.onload = function() {
             highlightActiveLink();
-            var navLinks = document.querySelectorAll('#header nav a, #footer a');
+            var navLinks = document.querySelectorAll('#header nav a');
             navLinks.forEach(function(link) {
                 link.addEventListener('click', smoothScroll);
             });
         };
     </script>
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
 <div id="header">
@@ -262,10 +272,10 @@ $memoires = $stmt->fetchAll();
         </table>
     </section>
 </div>
+  <div id="footer">
+        <p>© MemoPublish 2023-2024 - Pour tout problème de consultation ou si vous voulez publier un mémoire: <a href="mailto:memopublish@gmail.com">memopublish@gmail.com</a></p>
+    </div>
 
-<div id="footer">
-    <p>© MemoPublish 2023-2024 - Pour tout problème de consultation ou si vous voulez publier un mémoire: <a href="mailto:memopublish@gmail.com">memopublish@gmail.com</a></p>
-</div>
 
 </body>
 </html>
